7aa886de31b526f5b994ca7a1610552f1e4839d4
Stephen Yang <112423397+Stephenyeah@users.noreply.github.com>
2024-03-07 01:12:14 +0200

    Add security(login) function

--- src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
@@ -4,6 +4,9 @@ import fi.haagahelia.bookstore.domain.Book;
 import fi.haagahelia.bookstore.domain.BookRepository;
 import fi.haagahelia.bookstore.domain.Category;
 import fi.haagahelia.bookstore.domain.CategoryRepository;
+import fi.haagahelia.bookstore.domain.User;
+import fi.haagahelia.bookstore.domain.UserRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +14,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -22,8 +26,16 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	/**
+	 * Save demo users, courses and students to H2 DB
+	 * 
+	 * @param repository
+	 * @return
+	 */
+
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository drepository) {
+	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository drepository,
+			UserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple of category");
 			Stream.of("Fiction", "Mystery", "Romance", "Science Fiction", "Fantasy", "Biography").forEach(name -> {
@@ -41,6 +53,9 @@ public class BookstoreApplication {
 				log.info(book.toString());
 			}
 
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$08$bCCcGjB03eulCWt3CY0AZew2rVzXFyouUolL5dkL/pBgFkUH9O4J2", "ADMIN");
+			urepository.saveAll(Arrays.asList(user1, user2));
 		};
 	}
 }
